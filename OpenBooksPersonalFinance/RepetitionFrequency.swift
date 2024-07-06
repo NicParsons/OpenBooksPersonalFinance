59cb7eca7c84a346df2c70606eeaf852dc5486a6
NicParsons <96993843+NicParsons@users.noreply.github.com>
2024-07-07 00:06:52 +1000

    First commmit
    * add structs for Transaction, Account, and RepititionFrequency
    * add enum for Timeperiod

---
+++ OpenBooksPersonalFinance/RepetitionFrequency.swift
@@ -0,0 +1,8 @@
+//
+//  RepetitionFrequency.swift
+//  OpenBooksPersonalFinance
+//
+//  Created by Nicholas Parsons on 6/7/2024.
+//
+
+import Foundation
---
+++ OpenBooksPersonalFinance/Transaction.swift
@@ -0,0 +1,8 @@
+//
+//  Transaction.swift
+//  OpenBooksPersonalFinance
+//
+//  Created by Nicholas Parsons on 6/7/2024.
+//
+
+import Foundation
