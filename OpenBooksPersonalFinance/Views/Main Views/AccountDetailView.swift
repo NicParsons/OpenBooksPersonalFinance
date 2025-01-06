accaef4fe384b874d6037de7eff2e96dd82bda1b
NicParsons <96993843+NicParsons@users.noreply.github.com>
2025-01-06 16:30:32 +1100

    Add OpeningBalance to AccountDetailView

--- OpenBooksPersonalFinance/Views/Main Views/AccountDetailView.swift
+++ OpenBooksPersonalFinance/Views/Main Views/AccountDetailView.swift
@@ -16,6 +16,21 @@ struct AccountDetailView: View {
 
 				TextField("Account name", text: $account.name)
 
+				if let balance = account.openingBalance {
+					let openingBalanceBinding = Binding(
+						get: { account.openingBalance! },
+						set: { account.openingBalance = $0 }
+					) // end binding declaration
+
+					CurrencyAmount(title: "Opening balance",
+								   amount: openingBalanceBinding.amount,
+								   currency: openingBalanceBinding.currency,
+								   prompt: "The account's opening balance",
+								   canChangeCurrency: true)
+
+					DatePicker("Date opened", selection: openingBalanceBinding.date, displayedComponents: .date)
+				} // end if has opening balance
+
 					Toggle("Hidden", isOn: $account.hidden)
 
 				Toggle("Deletable", isOn: $account.isDeletable)
